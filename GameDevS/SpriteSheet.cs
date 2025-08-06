096a7138a304e2dd8505224e4396df283cc298a4
Lowie <lowie.klaasen@gmail.com>
2025-08-06 11:37:07 +0200

    .

---
+++ GameDevS/SpriteSheet.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace GameDevS
+{
+    internal class SpriteSheet
+    {
+        public Texture2D Texture;
+        public int NumberOfWidthSprites;
+        public int NumberOfHeightSprites;
+
+        public SpriteSheet(Texture2D texture, int numberOfWidthSprites, int numberOfHeightSprites)
+        {
+            Texture = texture;
+            NumberOfWidthSprites = numberOfWidthSprites;
+            NumberOfHeightSprites = numberOfHeightSprites;
+        }
+    }
+}
